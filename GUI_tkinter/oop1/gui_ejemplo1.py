98990df846205893538de9796b1e1bece54c8113
jarsmp37 <133422734+jarsmp37@users.noreply.github.com>
2025-02-17 08:20:12 -0600

    asda

--- GUI_tkinter/oop1/gui_ejemplo1.py
+++ GUI_tkinter/oop1/gui_ejemplo1.py
@@ -11,6 +11,19 @@ def registrar_usuario():
     entnombre.delete(0,tk.END)
     entedad.delete(0,tk.END)
 
+def mostrarusuarios():
+    ventana_usuarios=tk.Toplevel(ventana1)
+    ventana_usuarios.title("Usuarios Registrados")
+
+    #Jalar lista de la clase
+    usuarios=personas.obtener_lista()
+
+    #ciclo for para imprimir los datos
+    for i, usuario in enumerate(usuarios,start=1):
+        etiqueta_usuario=Label(ventana_usuarios,text=usuario.mostrardatos())
+        etiqueta_usuario.pack(pady=7)
+
+
 ventana1=tk.Tk()
 ventana1.title("Registro de usuarios")
 ventana1.geometry("500x400")
@@ -27,5 +40,8 @@ entedad.pack(pady=4)
 
 boton_reg=Button(ventana1,text="Registrar",command=registrar_usuario)
 boton_reg.pack(pady=6)
+
+boton_mostrar=Button(ventana1,text="Mostrar usuarios",command=mostrarusuarios)
+boton_mostrar.pack(pady=6)
 
 ventana1.mainloop()
