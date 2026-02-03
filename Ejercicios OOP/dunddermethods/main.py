4771cfb96a51fc7136d049728750fc7f5e87aa47
jarsmp37 <133422734+jarsmp37@users.noreply.github.com>
2026-02-03 16:07:10 -0600

    dsfa

---
+++ Ejercicios OOP/dunddermethods/main.py
@@ -0,0 +1,12 @@
+from atributos import *
+
+alu1=Nombre("Wendy",18,"Ciencias de la compu")
+alu2=Nombre("Ricardo",18,"Ciencia de datos")
+alu3=Nombre("Raul",19,"Ciencia de datos")
+
+print(alu2+alu3)
+print(alu1*alu2)
+print(alu1==alu3)
+
+print(str(alu1))
+print(repr(alu1))
